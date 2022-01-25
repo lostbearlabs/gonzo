e9ebff9569c649feea231404949f9a9775497bb4
eric.johnson <eric.johnson@code42.com>
2022-01-25 10:09:37 -0600

    add help command, refactor into command list, refactor into WorkingRepo class

--- app/src/main/kotlin/com/lostbearlabs/gonzo/App.kt
+++ app/src/main/kotlin/com/lostbearlabs/gonzo/App.kt
@@ -3,254 +3,58 @@ package com.lostbearlabs.gonzo
 import org.apache.log4j.BasicConfigurator
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
-import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.api.ListBranchCommand.ListMode
-import org.eclipse.jgit.api.errors.NotMergedException
-import org.eclipse.jgit.lib.*
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder
-import java.io.File
-import java.util.concurrent.TimeUnit
+import kotlin.system.exitProcess
 
 
 // TODO:
 //  - allow create branch (replaces gcplb)
 //  - allow commit changes (replaces gca)
 
+data class Command(val cmd: String, val title: String, val params: List<String>, val fn: (repo: WorkingRepo, List<String>) -> Unit)
+
+val commands: List<Command> = listOf(
+        Command("ls", "show branches", listOf()) { repo: WorkingRepo, ar: List<String> -> repo.showBranches() },
+        Command("c", "select branch", listOf("Branch")) { repo: WorkingRepo, ar: List<String> -> repo.pickBranch(ar[1]) },
+        Command("d", "delete branch", listOf("Branch")) { repo: WorkingRepo, ar: List<String> -> repo.deleteBranch(ar[1]) },
+        Command("f", "fetch", listOf()) { repo: WorkingRepo, ar: List<String> -> repo.fetch() },
+        Command("pd", "pull", listOf()) { repo: WorkingRepo, ar: List<String> -> repo.pull() },
+        Command("pu", "push", listOf()) { repo: WorkingRepo, ar: List<String> -> repo.push() },
+        Command("g", "delete gone branches", listOf()) { repo: WorkingRepo, ar: List<String> -> repo.deleteGone() },
+        Command("?", "show usage", listOf()) { repo: WorkingRepo, ar: List<String> -> printHelp() },
+        Command("q", "quit", listOf()) { repo: WorkingRepo, ar: List<String> -> exitProcess(0) }
+)
+
+fun printHelp() {
+    println("Usage: ")
+    commands.forEach {
+        println("  ${it.cmd} ${it.params.joinToString(" ")} : ${it.title}")
+    }
+    println()
+}
+
 fun main(args: Array<String>) {
     BasicConfigurator.configure()
     Logger.getRootLogger().level = Level.OFF
 
-    val workingDirectory = System.getProperty("user.dir")
-    val repo: Repository = FileRepositoryBuilder.create(File(workingDirectory, ".git"))
+    val repo = WorkingRepo()
     repo.use {
-        val git = Git(repo)
-        git.use {
+        repo.showBranches()
 
-            showBranches(git)
-
-            while (true) {
-                print("?> ")
-                val cmd = readLine()
+        while (true) {
+            print("?> ")
+            val cmd = readLine()
+            if (cmd != null) {
+                val ar = cmd.split(" ")
+                val cmd = commands.find { it.cmd == ar[0] }
                 if (cmd != null) {
-                    val ar = cmd.split(" ")
-                    when (ar[0]) {
-                        "q" -> break
-                        "ls" -> showBranches(git)
-                        "c" -> pickBranch(ar, git)
-                        "d" -> deleteBranch(ar, git)
-                        "f" -> fetch(git)
-                        "pd" -> pull(git)
-                        "pu" -> push(git)
-                        "g" -> deleteGone(git)
+                    if (ar.size != cmd.params.size + 1) {
+                        println("Wrong # args:  ${cmd.title} ${cmd.params.joinToString(" ")}")
+                        continue
                     }
+                    cmd.fn(repo, ar)
                 }
             }
         }
-    }
-}
-
-fun deleteGone(git: Git)  {
-    // TODO: call fetch() here once it's working!
-
-    val currentBranch = git.repository.fullBranch
-    val branches = getBranches(git).filter{ isGone(git, it.name) && it.name!=currentBranch }
-    if( branches.isEmpty()) {
-        println("NO GONE BRANCHES TO DELETE")
-        return
-    }
-
-    println("Deleting GONE branches:")
-    branches.forEach {
-        val shortBranchName = Repository.shortenRefName(it.name)
-        println("   $shortBranchName")
-    }
-
-    print("Confirm delete?  Y/[N] ")
-    val resp = readLine()
-    if( resp=="Y") {
-        branches.forEach {
-            deleteBranch(git, it)
-        }
-    }
-
-    showBranches(git)
-}
-
-// https://stackoverflow.com/a/41495542/4540
-fun String.runCommand(workingDir: File) {
-    ProcessBuilder(*split(" ").toTypedArray())
-            .directory(workingDir)
-            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
-            .start()
-            .waitFor(5, TimeUnit.MINUTES)
-
-}
-
-fun fetch(git: Git) {
-    println("fetch ...")
-
-    // TODO: ideally would just use jgit fetch, but I'm having SSH config issues?
-    "git fetch -p --all --quiet".runCommand(git.repository.directory.parentFile)
-
-    println("... fetch done")
-    showBranches(git)
-}
-
-fun pull(git: Git) {
-    println("pull ...")
-
-    // TODO: ideally would just use jgit pull, but I'm having SSH config issues?
-    "git pull -p --quiet".runCommand(git.repository.directory.parentFile)
-
-    println("... pull done")
-    showBranches(git)
-}
-
-fun push(git: Git)  {
-    println("push ...")
-
-    val currentBranch = git.repository.fullBranch
-    val shortBranchName = Repository.shortenRefName(currentBranch)
-
-    // TODO: ideally would just use jgit push, but I'm having SSH config issues?
-    "git push --set-upstream origin $shortBranchName".runCommand(git.repository.directory.parentFile)
-
-    println("... pull done")
-    showBranches(git)
-}
-
-fun pickBranch(ar: List<String>, git: Git) {
-    if (ar.size != 2) {
-        println("usage: c N")
-        return
-    }
-
-    var idx: Int
-    try {
-        idx = ar[1].toInt()
-    } catch (ex: NumberFormatException) {
-        idx = -1
-    }
-
-    val branches = getBranches(git)
-    if (idx < 0 || idx > ar.size) {
-        println("bad index $idx")
-        return
-    }
-
-    val ref = branches[idx]
-    git.checkout().setName(ref.name).call()
-
-    showBranches(git)
-}
-
-fun deleteBranch(ar: List<String>, git: Git) {
-    if (ar.size != 2) {
-        println("usage: d N")
-        return
-    }
-
-    var idx: Int
-    try {
-        idx = ar[1].toInt()
-    } catch (ex: NumberFormatException) {
-        idx = -1
-    }
-
-    val branches = getBranches(git)
-    if (idx < 0 || idx > ar.size) {
-        println("bad index $idx")
-        return
-    }
-
-    val currentBranch = git.repository.fullBranch
-    val ref = branches[idx]
-    if (ref.name == currentBranch) {
-        println("cannot delete selected branch")
-        return
-    }
-
-    if (!deleteBranch(git, ref)) return
-
-    showBranches(git)
-}
-
-private fun deleteBranch(git: Git, ref: Ref): Boolean {
-    try {
-        git.branchDelete().setBranchNames(ref.name).call()
-    } catch (ex: NotMergedException) {
-        val shortBranchName = Repository.shortenRefName(ref.name)
-        println("branch $shortBranchName not merged, delete anyway? Y/[N] ")
-        val cmd = readLine()
-        if (cmd == "Y") {
-            git.branchDelete().setBranchNames(ref.name).setForce(true).call()
-        } else {
-            return false
-        }
-    }
-    return true
-}
-
-
-fun showBranches(git: Git) {
-
-    val branches = getBranches(git)
-
-    val currentBranch = git.repository.fullBranch
-
-    println()
-    for ((n, refBranch) in branches.withIndex()) {
-        val prefix = if (refBranch.name == currentBranch) " * " else "   "
-        println("$n)  $prefix  ${refBranch.localName()}")
-
-        if( isGone(git, refBranch.name)) {
-            val shortBranchName = Repository.shortenRefName(refBranch.name)
-            val config = BranchConfig(git.repository.config,
-                    shortBranchName)
-            println("         -> (GONE) ${config.trackingBranch}")
-        } else {
-            val status = BranchTrackingStatus.of(git.repository, refBranch.name)
-            if (status != null) {
-                println("         -> ${status.remoteTrackingBranch}, ahead ${status.aheadCount}, behind ${status.behindCount}")
-            }
-        }
-    }
-}
-
-private fun isGone(git: Git, fullBranchName: String) : Boolean {
-    val shortBranchName = Repository.shortenRefName(fullBranchName)
-    val config = BranchConfig(git.repository.config, shortBranchName)
-
-    val trackingBranch: String? = config.trackingBranch
-    if( trackingBranch!=null ) {
-        git.repository.exactRef(trackingBranch) ?: return true
-    }
-    return false
-}
-
-private fun getBranches(git: Git): List<Ref> {
-    val listRefsBranches = git.branchList().setListMode(ListMode.ALL).call()
-            .filter { it.isLocal() }
-            .sortedBy { it.sortName() }
-    return listRefsBranches
-}
-
-
-fun Ref.isLocal(): Boolean {
-    return this.name.startsWith("refs/heads/")
-}
-
-fun Ref.localName(): String {
-    return this.name.removePrefix("refs/heads/")
-}
-
-fun Ref.sortName(): String {
-    return when (this.name) {
-        "refs/heads/develop" -> "_0${this.name}"
-        "refs/heads/main" -> "_1${this.name}"
-        "refs/heads/master" -> "_2${this.name}"
-        else -> this.name
     }
 }
 
---
+++ app/src/main/kotlin/com/lostbearlabs/gonzo/WorkingRepo.kt
@@ -0,0 +1,218 @@
+package com.lostbearlabs.gonzo
+
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.ListBranchCommand
+import org.eclipse.jgit.api.errors.NotMergedException
+import org.eclipse.jgit.lib.BranchConfig
+import org.eclipse.jgit.lib.BranchTrackingStatus
+import org.eclipse.jgit.lib.Ref
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import java.io.File
+import java.util.concurrent.TimeUnit
+
+class WorkingRepo : AutoCloseable {
+    private val workingDirectory : String = System.getProperty("user.dir")
+    private val repo: Repository = FileRepositoryBuilder.create(File(workingDirectory, ".git"))
+    private val git: Git = Git(repo)
+
+    override fun close() {
+        this.git.close()
+        this.repo.close()
+    }
+
+    fun deleteGone()  {
+        this.fetch()
+
+        val currentBranch = git.repository.fullBranch
+        val branches = this.getBranches().filter{ this.isGone(it.name) && it.name!=currentBranch }
+        if( branches.isEmpty()) {
+            println("NO GONE BRANCHES TO DELETE")
+            return
+        }
+
+        println("Deleting GONE branches:")
+        branches.forEach {
+            val shortBranchName = Repository.shortenRefName(it.name)
+            println("   $shortBranchName")
+        }
+
+        print("Confirm delete?  Y/[N] ")
+        val resp = readLine()
+        if( resp=="Y") {
+            branches.forEach {
+                this.deleteBranch(it)
+            }
+        }
+
+        this.showBranches()
+    }
+
+    // https://stackoverflow.com/a/41495542/4540
+    private fun String.runCommand(workingDir: File) {
+        ProcessBuilder(*split(" ").toTypedArray())
+                .directory(workingDir)
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start()
+                .waitFor(5, TimeUnit.MINUTES)
+
+    }
+
+    fun fetch() {
+        println("fetch ...")
+
+        // TODO: ideally would just use jgit fetch, but I'm having SSH config issues?
+        "git fetch -p --all --quiet".runCommand(git.repository.directory.parentFile)
+
+        println("... fetch done")
+        this.showBranches()
+    }
+
+    fun pull() {
+        println("pull ...")
+
+        // TODO: ideally would just use jgit pull, but I'm having SSH config issues?
+        "git pull -p --quiet".runCommand(git.repository.directory.parentFile)
+
+        println("... pull done")
+        this.showBranches()
+    }
+
+    fun push()  {
+        println("push ...")
+
+        val currentBranch = git.repository.fullBranch
+        val shortBranchName = Repository.shortenRefName(currentBranch)
+
+        // TODO: ideally would just use jgit push, but I'm having SSH config issues?
+        "git push --set-upstream origin $shortBranchName".runCommand(git.repository.directory.parentFile)
+
+        println("... pull done")
+        this.showBranches()
+    }
+
+    fun pickBranch(idxString: String) {
+        val idx: Int = try {
+            idxString.toInt()
+        } catch (ex: NumberFormatException) {
+            -1
+        }
+
+        val branches = this.getBranches()
+        if (idx < 0 || idx >= branches.size) {
+            println("bad index $idx")
+            return
+        }
+
+        val ref = branches[idx]
+        git.checkout().setName(ref.name).call()
+
+        this.showBranches()
+    }
+
+    fun deleteBranch(idxString: String) {
+
+        val idx: Int = try {
+            idxString.toInt()
+        } catch (ex: NumberFormatException) {
+            -1
+        }
+
+        val branches = getBranches()
+        if (idx < 0 || idx >= branches.size) {
+            println("bad index $idx")
+            return
+        }
+
+        val currentBranch = git.repository.fullBranch
+        val ref = branches[idx]
+        if (ref.name == currentBranch) {
+            println("cannot delete selected branch")
+            return
+        }
+
+        if (!this.deleteBranch(ref)) return
+
+        this.showBranches()
+    }
+
+    private fun deleteBranch(ref: Ref): Boolean {
+        try {
+            git.branchDelete().setBranchNames(ref.name).call()
+        } catch (ex: NotMergedException) {
+            val shortBranchName = Repository.shortenRefName(ref.name)
+            println("branch $shortBranchName not merged, delete anyway? Y/[N] ")
+            val cmd = readLine()
+            if (cmd == "Y") {
+                git.branchDelete().setBranchNames(ref.name).setForce(true).call()
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+
+
+    fun showBranches() {
+
+        val branches = this.getBranches()
+
+        val currentBranch = git.repository.fullBranch
+
+        println()
+        for ((n, refBranch) in branches.withIndex()) {
+            val prefix = if (refBranch.name == currentBranch) " * " else "   "
+            println("$n)  $prefix  ${refBranch.localName()}")
+
+            if( this.isGone(refBranch.name)) {
+                val shortBranchName = Repository.shortenRefName(refBranch.name)
+                val config = BranchConfig(git.repository.config,
+                        shortBranchName)
+                println("         -> (GONE) ${config.trackingBranch}")
+            } else {
+                val status = BranchTrackingStatus.of(git.repository, refBranch.name)
+                if (status != null) {
+                    println("         -> ${status.remoteTrackingBranch}, ahead ${status.aheadCount}, behind ${status.behindCount}")
+                }
+            }
+        }
+    }
+
+    private fun isGone(fullBranchName: String) : Boolean {
+        val shortBranchName = Repository.shortenRefName(fullBranchName)
+        val config = BranchConfig(git.repository.config, shortBranchName)
+
+        val trackingBranch: String? = config.trackingBranch
+        if( trackingBranch!=null ) {
+            git.repository.exactRef(trackingBranch) ?: return true
+        }
+        return false
+    }
+
+    private fun getBranches(): List<Ref> {
+        return git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call()
+                .filter { it.isLocal() }
+                .sortedBy { it.sortName() }
+    }
+
+
+    private fun Ref.isLocal(): Boolean {
+        return this.name.startsWith("refs/heads/")
+    }
+
+    private fun Ref.localName(): String {
+        return this.name.removePrefix("refs/heads/")
+    }
+
+    private fun Ref.sortName(): String {
+        return when (this.name) {
+            "refs/heads/develop" -> "_0${this.name}"
+            "refs/heads/main" -> "_1${this.name}"
+            "refs/heads/master" -> "_2${this.name}"
+            else -> this.name
+        }
+    }
+
+
+}
