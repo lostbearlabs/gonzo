8e009954daac6c7117eef2b60153acf7f8467063
eric.johnson <eric.johnson@code42.com>
2022-02-04 10:38:19 -0600

    add check.dotfiles

--- app/src/main/kotlin/com/lostbearlabs/gonzo/App.kt
+++ app/src/main/kotlin/com/lostbearlabs/gonzo/App.kt
@@ -6,23 +6,23 @@ import org.apache.log4j.Logger
 import kotlin.system.exitProcess
 
 
-data class Command(val cmd: String, val title: String, val params: List<String>, val fn: (repo: WorkingRepo, List<String>) -> Unit)
+data class Command(val cmd: String, val title: String, val params: List<String>, val fn: (List<String>) -> Unit)
 
 val commands: List<Command> = listOf(
-        Command("ls", "show branches", listOf()) { repo: WorkingRepo, _: List<String> -> repo.showBranches() },
-        Command("c", "choose branch", listOf("Branch")) { repo: WorkingRepo, ar: List<String> -> repo.pickBranch(ar[1]) },
-        Command("d", "delete branch", listOf("Branch")) { repo: WorkingRepo, ar: List<String> -> repo.deleteBranch(ar[1]) },
-        Command("f", "fetch all", listOf()) { repo: WorkingRepo, _: List<String> -> repo.fetchAll() },
-        Command("pd", "pull down", listOf()) { repo: WorkingRepo, _: List<String> -> repo.pull() },
-        Command("pu", "push up", listOf()) { repo: WorkingRepo, _: List<String> -> repo.push() },
-        Command("dg", "delete gone branches", listOf()) { repo: WorkingRepo, _: List<String> -> repo.deleteGone() },
-        Command("br", "create branch", listOf("BranchName")) { repo: WorkingRepo, ar: List<String> -> repo.createBranch(ar[1]) },
-        Command("bt", "create ticket branch", listOf("TicketId", "BranchName")) { repo: WorkingRepo, ar: List<String> -> repo.createTicketBranch(ar[1], ar[2]) },
-        Command("ca", "commit all", listOf("CommitMessage")) {repo: WorkingRepo, ar: List<String> -> repo.commitAll(ar[1]) },
-        Command("scan", "scan ~/dev for repos with uncommitted changes", listOf()) {_: WorkingRepo, _: List<String> -> ScanUncommitted().scan() },
-        Command("check.dotfiles", "check whether any cruft has crept into dotfiles", listOf()) { _: WorkingRepo, _: List<String> -> CheckDotFiles().check() },
-        Command("?", "show usage", listOf()) { _: WorkingRepo, _: List<String> -> printHelp() },
-        Command("q", "quit", listOf()) { _: WorkingRepo, _: List<String> -> exitProcess(0) }
+        Command("ls", "show branches", listOf()) { _ -> WorkingRepo().use { it.showBranches() } },
+        Command("c", "choose branch", listOf("Branch")) { ar: List<String> -> WorkingRepo().use { it.pickBranch(ar[1]) } },
+        Command("d", "delete branch", listOf("Branch")) { ar: List<String> -> WorkingRepo().use { it.deleteBranch(ar[1]) } },
+        Command("f", "fetch all", listOf()) { _ -> WorkingRepo().use { it.fetchAll() } },
+        Command("pd", "pull down", listOf()) { _ -> WorkingRepo().use { it.pull() } },
+        Command("pu", "push up", listOf()) { _ -> WorkingRepo().use { it.push() } },
+        Command("dg", "delete gone branches", listOf()) { _ -> WorkingRepo().use { it.deleteGone() } },
+        Command("br", "create branch", listOf("BranchName")) { ar: List<String> -> WorkingRepo().use { it.createBranch(ar[1]) } },
+        Command("bt", "create ticket branch", listOf("TicketId", "BranchName")) { ar: List<String> -> WorkingRepo().use { it.createTicketBranch(ar[1], ar[2]) } },
+        Command("ca", "commit all", listOf("CommitMessage")) { ar: List<String> -> WorkingRepo().use { it.commitAll(ar[1]) } },
+        Command("scan", "scan ~/dev for repos with uncommitted changes", listOf()) { ScanUncommitted().scan() },
+        Command("check.dotfiles", "check whether any cruft has crept into dotfiles", listOf()) { CheckDotFiles().check() },
+        Command("?", "show usage", listOf()) { printHelp() },
+        Command("q", "quit", listOf()) { exitProcess(0) }
 )
 
 fun printHelp() {
@@ -37,34 +37,30 @@ fun main(args: Array<String>) {
     BasicConfigurator.configure()
     Logger.getRootLogger().level = Level.OFF
 
-    val repo = WorkingRepo()
-    repo.use {
-
-        if (args.isNotEmpty()) {
-            runCommand(args.toList(), repo)
-        } else {
-            repo.showBranches()
-            while (true) {
-                print("?> ")
-                val cmd = readLine()
-                if (cmd != null) {
-                    val ar = cmd.split(" ")
-                    runCommand(ar, repo)
-                }
+    if (args.isNotEmpty()) {
+        runCommand(args.toList())
+    } else {
+        WorkingRepo().use{ it.showBranches() }
+        while (true) {
+            print("?> ")
+            val cmd = readLine()
+            if (cmd != null) {
+                val ar = cmd.split(" ")
+                runCommand(ar)
             }
         }
-
     }
+
 }
 
-private fun runCommand(ar: List<String>, repo: WorkingRepo) {
+private fun runCommand(ar: List<String>) {
     val cmd = commands.find { it.cmd == ar[0] }
     if (cmd != null) {
         if (ar.size != cmd.params.size + 1) {
-            println("Wrong # args:  ${cmd.cmd} ${cmd.params.joinToString(" ")}  (got ${ar.size-1}, expected ${cmd.params.size}")
+            println("Wrong # args:  ${cmd.cmd} ${cmd.params.joinToString(" ")}  (got ${ar.size - 1}, expected ${cmd.params.size}")
             return
         }
-        cmd.fn(repo, ar)
+        cmd.fn(ar)
     } else {
         println("Unknown command: ${ar[0]}")
     }
